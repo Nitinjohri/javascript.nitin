347fcc36b2bab07f866587d72cff6b86eca992b7
Nitinjohri <119477689+Nitinjohri@users.noreply.github.com>
2026-01-19 12:27:42 +0000

    initial commit

---
+++ 01-basics/01-datatype-conversion.js
@@ -0,0 +1,36 @@
+let score="33abc";
+let score1=null;
+let score2=undefined;
+let score3=true;
+//console.log(typeof score);
+//console.log(typeof (score));
+let valueInNumber= Number(score);
+let valueInNumber1=Number(score1);
+let valueInNumber2=Number(score2);
+let valueInNumber3=Number(score3);
+//console.log(valueInNumber3);
+//console.log(typeof valueInNumber3);
+//console.log(typeof valueInNumber2);
+//console.log(valueInNumber2);
+//console.log(typeof valueInNumber1);
+//console.log(valueInNumber1);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//"33" =>33
+//"33abc"=>NaN
+//NULL=>0
+//UNDEFINED =>NaN
+//TRUE=>1
+let isloggedIn="nitin";
+let booleanIsLoggedIn=Boolean(isloggedIn);
+//console.log(booleanIsLoggedIn);
+//"nitin"=>true
+//""=>false
+//0=>false
+//any number other than 0=>true
+//null=>false
+//undefined=>false
+let someNumber="33";
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);

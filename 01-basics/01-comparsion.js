f2e5e75622e989a2d1396768aaf7237760d9bfff
Nitinjohri <119477689+Nitinjohri@users.noreply.github.com>
2026-01-20 06:06:52 +0000

    initial commit

---
+++ 01-basics/01-comparsion.js
@@ -0,0 +1,20 @@
+/*
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+*/
+//console.log("2">1);
+//console.log("02">=1);
+
+// it can provide confusion while performing task
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
+
+// === it checks datatype also
+console.log("2"===2);
